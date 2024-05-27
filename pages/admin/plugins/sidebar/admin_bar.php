6d2617db4ee504015076e82ca8f730dc281f6d70
Khenneth <kentlanticse1@gmail.com>
2024-05-27 10:30:14 +0800

    updated 27/May/24

---
+++ pages/admin/plugins/sidebar/admin_bar.php
@@ -0,0 +1,55 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="index.php" class="brand-link">
+    <img src="../../dist/img/logo.ico" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">&ensp;WEB &ensp;|&ensp; Admin</span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="../../dist/img/user.png" class="img-circle elevation-2" alt="User Image">
+      </div>
+      <div class="info">
+        <a href="index.php" class="d-block"><?=htmlspecialchars($_SESSION['name']);?></a>
+      </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+        <li class="nav-item">
+          <?php if ($_SERVER['REQUEST_URI'] == "/k_temp/pages/admin/index.php") {?>
+          <a href="index.php" class="nav-link active">
+          <?php } else {?>
+          <a href="index.php" class="nav-link">
+          <?php } ?>
+            <i class="nav-icon fas fa-bus"></i>
+            <p>
+              Dashboard
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <?php if ($_SERVER['REQUEST_URI'] == "/k_temp/pages/admin/index2.php") {?>
+          <a href="index2.php" class="nav-link active">
+          <?php } else {?>
+          <a href="index2.php" class="nav-link">
+          <?php } ?>
+            <i class="nav-icon fas fa-question"></i>
+            <p>
+              Ano?
+            </p>
+          </a>
+        </li>
+        <?php include 'logout.php';?>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
