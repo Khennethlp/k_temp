7e84e4daed6417461e26eddefb05e03792ac15b7
khenneth <khennethlp@gmail.com>
2024-05-31 17:46:59 +0800

    viewer page update

--- pages/viewer/index.php
+++ pages/viewer/index.php
@@ -24,7 +24,27 @@ include('plugins/navbar/index_navbar.php');
 
     <!-- Main content -->
     <div class="content">
-      
+      <div class="col-md-12">
+          <div class="row m-3">
+          <div class="col-md-3 border border-danger">
+            <div class="p-2 ">
+              <p>Lorem Ipsum Dolor sit amit.</p>
+              <p>Lorem Ipsum Dolor sit amit.</p>
+              <p>Lorem Ipsum Dolor sit amit.</p>
+              <p>Lorem Ipsum Dolor sit amit.</p>
+              <p>Lorem Ipsum Dolor sit amit.</p>
+              <p>Lorem Ipsum Dolor sit amit.</p>
+              <p>Lorem Ipsum Dolor sit amit.</p>
+              <p>Lorem Ipsum Dolor sit amit.</p>
+              <p>Lorem Ipsum Dolor sit amit.</p>
+              <p>Lorem Ipsum Dolor sit amit.</p>
+            </div>
+          </div>
+          <div class="col-md-3 border border-info"></div>
+          <div class="col-md-3 border border-warning"></div>
+          <div class="col-md-3 border border-success"></div>
+        </div>
+      </div>
     </div>
     <!-- /.content -->
   </div>
